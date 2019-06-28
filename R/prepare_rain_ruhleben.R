b16e29486065336cdada7af7004084027a848259
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-06-28 13:28:37 +0200

    Report about setting high rain values to NA
    Also fix bug in prepare_rain_ruhleben(): pipe
    operator was missing!

--- R/prepare_rain_ruhleben.R
+++ R/prepare_rain_ruhleben.R
@@ -8,7 +8,7 @@
 prepare_rain_ruhleben <- function(combined)
 {
   combined %>%
-    clear_high_values_in_rain_columns(threshold = 20)
+    clear_high_values_in_rain_columns(threshold = 20) %>%
     add_average_rain_columns() %>%
     summarise_rain_ruhleben()
 }
@@ -17,11 +17,37 @@ prepare_rain_ruhleben <- function(combined)
 clear_high_values_in_rain_columns <- function(combined, threshold)
 {
   # Determine rain data columns
-  columns <- 3:(ncol(combined) - 1)
+  columns <- setdiff(names(combined), c("tBeg", "tEnd", "KW.Ruh"))
 
   # Set unplausible rain values to NA
-  combined[, columns] <- apply(combined[, columns], 2, function(x) {
-    ifelse(x > threshold, NA, x)
+  combined[columns] <- lapply(columns, function(column) {
+
+    x <- combined[[column]]
+
+    which_above <- which(x > threshold)
+    n_above <- length(which_above)
+
+    if (n_above > 0) {
+
+      x[which_above] <- NA
+
+      context <- -1:1
+      context_indices <- rep(which_above, each = length(context)) + context
+      context_indices <- unique(pmax(pmin(context_indices, length(x)), 0))
+
+      kwb.utils::printIf(
+        condition = TRUE,
+        x = kwb.utils::selectColumns(
+          combined[context_indices, ], c("tBeg", "tEnd", column)
+        ),
+        caption = sprintf(
+          "\nSetting %d values above %0.1f to NA in column '%s'",
+          n_above, threshold, column
+        )
+      )
+    }
+
+    x
   })
 
   combined
