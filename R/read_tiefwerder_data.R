e09bf8e257f1f6649b2bf99114afadb73961fba6
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-06-28 12:03:11 +0200

    Delete read_tiefwerder_data()

--- R/read_tiefwerder_data.R
+++
@@ -1,9 +0,0 @@
-# read_tiefwerder_data ---------------------------------------------------------
-read_tiefwerder_data <- function(file)
-{
-  TW <- utils::read.csv(file, sep = ";", header = TRUE, dec = ".")
-
-  TW$DateTime <- as.POSIXct(TW$DateTime)
-
-  TW
-}
