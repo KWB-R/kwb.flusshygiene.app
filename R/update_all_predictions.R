bdf2bde42e356d3ccc9f6f881f9438ad2a892ca9
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-07-02 13:41:04 +0200

    Do not pass "file" to update_all_predictions()
    The function knows where to find/put the file

--- R/update_all_predictions.R
+++ R/update_all_predictions.R
@@ -1,6 +1,10 @@
 # update_all_predictions -------------------------------------------------------
-update_all_predictions <- function(prediction_today, file)
+update_all_predictions <- function(prediction_today, root = get_root())
 {
+  paths <- create_folder_structure(root)
+
+  file <- file.path(paths$predictions, "Vorhersagen.csv")
+
   if (exists(file)) {
 
     all_predictions <- utils::read.csv(file, sep = ";")
@@ -12,6 +16,9 @@ update_all_predictions <- function(prediction_today, file)
     )
 
   } else {
+
+    # Check if target directory exists
+    kwb.utils::safePath(dirname(file))
 
     write_input_file(
       x = prediction_today,
